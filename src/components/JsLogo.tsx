55846a13c925b629081c56d9b18d306d3a3dd8c3
jstam2023 <jstam2023@gmail.com>
2025-08-10 16:31:12 +0300

    feat: add JsLogo

---
+++ src/components/JsLogo.tsx
@@ -0,0 +1,13 @@
+const JsLogo = () => {
+    return (
+        <>
+            <img
+                className="my-4 h-16"
+                src="/public/js.png"
+                alt="JsLogo"
+            />
+        </>
+    )
+}
+
+export default JsLogo;
