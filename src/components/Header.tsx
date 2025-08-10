b6eb912989acd17d2d9306987f64a9f7db4b6057
jstam2023 <jstam2023@gmail.com>
2025-08-10 18:21:46 +0300

    feat: add links to Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,16 @@ const Header = () => {
     return (
         <>
             <header className="bg-black">
-                <JsLogo />
+                <div className="container mx-auto flex items-center justify-between">
+                    <JsLogo />
+                    <div>
+                        <a href="/" className="text-white hover:underline hover:underline-offset-4 hover:text-red-400 pr-8">Home</a>
+                        <a href="/" className="text-white hover:underline hover:underline-offset-4 hover:text-red-400 pr-8">Apps</a>
+                    </div>
+
+
+                </div>
+
 
             </header>
         </>
