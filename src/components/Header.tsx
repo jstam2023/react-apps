ce94d9443db15bd462e5045b252ac68b9f5bdd9e
jstam2023 <jstam2023@gmail.com>
2025-08-10 16:32:20 +0300

    feat: add Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import JsLogo from "./JsLogo.tsx";
+
+const Header = () => {
+    return (
+        <>
+            <header className="bg-black">
+                <JsLogo />
+
+            </header>
+        </>
+    )
+}
+
+export default Header;
